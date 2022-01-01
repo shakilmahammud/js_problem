8ebfecf06ab25629bf5cb5d025a8d05e8df11e74
shakilmahammud <hshakil987@gmail.com>
2022-01-02 00:26:34 +0600

    try

--- vaxTrail.js
+++ vaxTrail.js
@@ -1,4 +1,20 @@
 
-function vaxTrail() {
-    
+function vaxTrail(obj) {
+    const isEven = num => num % 2 === 0;
+     return obj.sort(function (a, b) {
+        if(isEven(a.age) && !isEven(b.age)){
+            return -1;
+         };
+         if(!isEven(a.age) && isEven(b.age)){
+            return 1;
+         };
+         return a - b;
+        
+      });
 }
+
+console.log(vaxTrail([
+    { name: 'sunil', age: 21, temperature: 98 },
+    { name: 'Biplap', age: 22, temperature: 98 },
+    { name: 'Biplap', age: 36, temperature: 98 },
+    ]))
