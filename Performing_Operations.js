651c9de94848fd759cbd5a1a1fda5d6cb4476208
shakilmahammud <hshakil987@gmail.com>
2023-01-17 23:33:37 +0600

    feat:added

---
+++ Performing_Operations.js
@@ -0,0 +1,11 @@
+const finalValueAfterOperations = (operations) =>{
+    let x=0;
+    for(let i =0; i<operations.length;i++){
+        if((operations[i]=='++X')||(operations[i]=='X++')){
+            x+=1;
+        }else if((operations[i]=='--X')||(operations[i]=='X--')){
+            x-=1;
+        }
+    }
+    return x;
+};
