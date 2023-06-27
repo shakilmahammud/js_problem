64451f7bf06bd3ee71bbb46b8459362680d6c1c6
shakilmahammud <hshakil987@gmail.com>
2023-06-27 11:03:21 +0600

    added

--- sort.js
+++ sort.js
@@ -5,10 +5,10 @@
 // Example 1:
 
 // Input: arr = [5, 4, 1, 2, 3], fn = (x) => x
-// Output: [1, 2, 3, 4, 5]
+Output: [1, 2, 3, 4, 5]
 // Explanation: fn simply returns the number passed to it so the array is sorted in ascending order.
 
 
 var sortBy = function(arr, fn) {
-  return arr.sort((a,b)=>fn(a)-fn(b));
+ return arr.sort((a,b)=>fn(a)-fn(b));
 };
